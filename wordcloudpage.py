0fb63fb66c935552c99e7f2be1b60aae0c1cc886
Chanhyeok Song <kacu0417@naver.com>
2023-11-21 03:07:44 +0900

    test2

--- wordcloudpage.py
+++ wordcloudpage.py
@@ -5,11 +5,27 @@ import pandas as pd
 
 from wordcloud import WordCloud
 
-from utils import load_data
-
 import streamlit as st
 
 spark_session = SparkSession.builder.master("local").appName("article").getOrCreate()
+
+@st.cache_data
+def load_data():
+    #health_twitter = spark_session.sparkContext.textFile("./health_twitter/*.txt")
+    bbchealth = spark_session.sparkContext.textFile("./health_twitter/bbchealth.txt")
+    cbchealth = spark_session.sparkContext.textFile("./health_twitter/cbchealth.txt")
+    cnnhealth = spark_session.sparkContext.textFile("./health_twitter/cnnhealth.txt")
+    everydayhealth = spark_session.sparkContext.textFile("./health_twitter/everydayhealth.txt")
+    gdnhealthcare = spark_session.sparkContext.textFile("./health_twitter/gdnhealthcare.txt")
+    goodhealth = spark_session.sparkContext.textFile("./health_twitter/goodhealth.txt")
+    latimeshealth = spark_session.sparkContext.textFile("./health_twitter/latimeshealth.txt")
+    nprhealth = spark_session.sparkContext.textFile("./health_twitter/nprhealth.txt")
+    nytimeshealth = spark_session.sparkContext.textFile("./health_twitter/nytimeshealth.txt")
+    reuters_health = spark_session.sparkContext.textFile("./health_twitter/reuters_health.txt")
+    usnewshealth = spark_session.sparkContext.textFile("./health_twitter/usnewshealth.txt")
+    health_twitter = bbchealth.union(cbchealth).union(cnnhealth).union(everydayhealth).union(gdnhealthcare).union(goodhealth).union(latimeshealth).union(nprhealth).union(nytimeshealth).union(reuters_health).union(usnewshealth)
+
+    return health_twitter
 
 def wordcounts(x): 
     rdd=x.filter(lambda line: len(line) > 0) \
